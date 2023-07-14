e23f66fdda587e83e7d72edfc8247b62e17191c9
memaheshd <memaheshd@gmail.com>
2023-07-14 21:25:10 +0545

    form validation using regular expression

--- javascript/formvalidation.js
+++ javascript/formvalidation.js
@@ -1,19 +1,23 @@
-const displayMsg =(message,id,colorName)=>{
-    document.getElementById(id).innerHTML=message
-    document.getElementById(id).style.color=colorName
+const displayMsg = (message, id, colorName) => {
+    document.getElementById(id).innerHTML = message
+    document.getElementById(id).style.color = colorName
 
 }
-const fnameValidate = () => {
+const fnameValidate  = () => {
     const first_name = document.getElementById('fname').value
     if (first_name == "") {
         displayMsg('first name is mandatory', 'fnameMsg', 'red')
+        return false
+    }
+    else if (!first_name.match(/^([a-z])+$/)) {
+        displayMsg('first name only contains lowercase alphabets', 'fnameMsg', 'red')
         return false
     }
     else if (first_name.length < 3) {
         displayMsg('first name must be more then 2 characters', 'fnameMsg', 'red')
         return false
     }
-    else{
+    else {
         displayMsg('valid firstname', 'fnameMsg', 'green')
         return true
     }
@@ -21,14 +25,18 @@ const fnameValidate = () => {
 const lnameValidate = () => {
     const last_name = document.getElementById('lname').value
     if (last_name == "") {
-        displayMsg('last name is mandatory', 'lnameMsg','red')
+        displayMsg('last name is mandatory', 'lnameMsg', 'red')
+        return false
+    }
+    else if (!last_name.match(/^([a-z])+$/)) {
+        displayMsg('last name only contains lowercase letters only', 'lnameMsg', 'red')
         return false
     }
     else if (last_name.length < 3) {
-        displayMsg('last name must be more then 2 characters', 'lnameMsg','red')
+        displayMsg('last name must be more then 2 characters', 'lnameMsg', 'red')
         return false
     }
-    else{
+    else {
         displayMsg('valid lastname', 'lnameMsg', 'green')
         return true
     }
@@ -38,14 +46,14 @@ const emailValidate = () => {
     const email = document.getElementById('email').value
 
     if (email == "") {
-        displayMsg('email is mandatory', 'emailMsg','red')
+        displayMsg('email is mandatory', 'emailMsg', 'red')
         return false
     }
-    else if (email.length < 3) {
-        displayMsg('email must be more then 2 characters', 'emailMsg','red')
+    else if (!email.match(/^([a-zA-Z0-9])([a-z0-9\-\_\.])+\@+([a-z])+\.+([a-z])+$/)) {
+        displayMsg('invalid email format', 'emailMsg', 'red')
         return false
     }
-    else{
+    else {
         displayMsg('valid email', 'emailMsg', 'green')
         return true
     }
@@ -54,31 +62,41 @@ const emailValidate = () => {
 const pwdValidate = () => {
     const password = document.getElementById('pwd').value
     if (password == "") {
-        displayMsg('password is mandatory', 'pwdMsg','red')
+        displayMsg('password is mandatory', 'pwdMsg', 'red')
         return false
     }
-    else if (password.length < 8) {
-        displayMsg('password must be more then 7 characters', 'pwdMsg','red')
+    else if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\@\#\$\-\_\?]).{8,100}$/)) {
+        displayMsg('password be minimun of 8 character and should be like: Abc@123', 'pwdMsg', 'red')
         return false
     }
-    else{
-        displayMsg('valid password', 'pwdMsg', 'green')
+    else {
+        displayMsg('strong password', 'pwdMsg', 'green')
         return true
     }
 }
 
 const cpwdValidate = () => {
+    const password = document.getElementById('pwd').value
     const confirmPassword = document.getElementById('cpwd').value
     if (confirmPassword == "") {
-        displayMsg('confirm password is mandatory', 'cpwdMsg','red')
+        displayMsg('confirm password is mandatory', 'cpwdMsg', 'red')
         return false
     }
-    else if (confirmPassword.length < 8) {
-        displayMsg('confirm password must be more then 7 characters', 'cpwdMsg','red')
+    else if (password !== confirmPassword) {
+        displayMsg('password and confirm password is not matche: Abc@123', 'pwdMsg', 'red')
         return false
     }
-    else{
-        displayMsg('valid confirm password', 'cpwdMsg', 'green')
+    else {
+        displayMsg('password and confirm password matchs', 'cpwdMsg', 'green')
         return true
+    }
+}
+
+const validForm=() => {
+    if (fnameValidate() && lnameValidate() && emailValidate() && pwdValidate() && cpwdValidate()) {
+        return true
+    }
+    else {
+        return false
     }
 }
