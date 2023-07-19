8fdfdbbe028b402cbddc18872bc7289c7d9a7368
memaheshd <memaheshd@gmail.com>
2023-07-19 14:57:25 +0545

    Dispaly is added to MyRoute.js to diaplay content of dispaly in localhost:3000/try as path

--- react/demo-app/src/MyRoute.js
+++ react/demo-app/src/MyRoute.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import First from './First'
 import { Second } from './Second'
+import Display from './Display'
 const MyRoute = () => {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<First/>}/>
                 <Route path='/test' element={<Second/>}/>
+                <Route path='/try' element={<Display/>}/>
             </Routes>
         </Router>
 
