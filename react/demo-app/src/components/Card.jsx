98c44a3caafda1cb9f945d4876a451b9fee5948e
memaheshd <memaheshd@gmail.com>
2023-07-21 14:26:03 +0545

    adding images inside public folder

---
+++ react/demo-app/src/components/Card.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <>
+            <div className="contaner_fluid">
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <img src="..." className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <button className='btn btn-success'> View Details</button>
+                                </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <img src="..." className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <button className='btn btn-success'> View Details</button>
+                                </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <img src="..." className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <button className='btn btn-success'> View Details</button>
+                                </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <img src="..." className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <button className='btn btn-success'> View Details</button>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
