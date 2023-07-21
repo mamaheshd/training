6b21763da01d42deb493d7e9557c92eb4ba0d883
memaheshd <memaheshd@gmail.com>
2023-07-21 14:52:40 +0545

    adding carousel

---
+++ react/demo-app/src/components/Carousel.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Carousel = () => {
+    return (
+        <>
+            <div id="carouselExample" className="carousel slide">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="/images/c1.jpg" className="d-block w-100" alt="..."/>
+                    </div>
+                    <div className="carousel-item">
+                        <img src="/images/c2.jpg" className="d-block w-100" alt="..."/>
+                    </div>
+                    <div className="carousel-item">
+                        <img src="/images/c3.jpg" className="d-block w-100" alt="..."/>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default Carousel
--- react/demo-app/src/pages/Homepage.jsx
+++ react/demo-app/src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Card from '../components/Card'
+import Carousel from '../components/Carousel'
 
 const Homepage = () => {
   return (
     <>
+      <Carousel/>
       <Card/>
       <Card/>
     </>
