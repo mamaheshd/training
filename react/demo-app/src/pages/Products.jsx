19580d9de48ca7ff02068fc5ef9931a35816f384
memaheshd <memaheshd@gmail.com>
2023-07-24 14:54:43 +0545

    card is manged in products

--- react/demo-app/src/pages/Products.jsx
+++ react/demo-app/src/pages/Products.jsx
@@ -9,16 +9,20 @@ const Products = () => {
   useEffect(() => {//sideeffect that seen when page is loading in local
     // alert('Hello it is use effect')
     axios.get(`https://fakestoreapi.com/products`)
-    .then(res=>{ //res is name it may be any
-      console.log(res.data)
-      setProducts(res.data)
-    })
-    .catch(err=>console.log(err))
-  },[]) // empty array is passed. it dont load when state is changed
+      .then(res => { //res is name it may be any
+        console.log(res.data)
+        setProducts(res.data)
+      })
+      .catch(err => console.log(err))
+  }, []) // empty array is passed. it dont load when state is changed
   return (
     <>
-      {/* <Card />
-      {products.map(item=>{
+      <div className="contaner_fluid">
+        <div className="row row-cols-1 row-cols-md-4 g-4">
+          <Card />
+        </div>
+      </div>
+      {/* {products.map(item=>{
         return(
           <>
           <h1>{item.title}</h1>
