3ecc17c76eee2d8698572d90d3824bbe792750b5
memaheshd <memaheshd@gmail.com>
2023-07-24 14:33:25 +0545

    using useEffect and useState in Products.jsx files   small change

--- react/demo-app/src/pages/Products.jsx
+++ react/demo-app/src/pages/Products.jsx
@@ -6,7 +6,7 @@ const Products = () => {
   const [products, setProducts] = useState([])
   // products is variable and 
   // setProducts is function it name may be different
-  useEffect(() => {//sideeffect that seen when page is loading
+  useEffect(() => {//sideeffect that seen when page is loading in local
     alert('Hello it is use effect')
   })
   return (
