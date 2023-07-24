ba53e5aba0a0d3131d912e25f3bb0b8353976e6b
memaheshd <memaheshd@gmail.com>
2023-07-24 14:48:51 +0545

    jeson data is fatch from API and display title of product

--- react/demo-app/src/pages/Products.jsx
+++ react/demo-app/src/pages/Products.jsx
@@ -7,11 +7,22 @@ const Products = () => {
   // products is variable and 
   // setProducts is function it name may be different
   useEffect(() => {//sideeffect that seen when page is loading in local
-    alert('Hello it is use effect')
-  })
+    // alert('Hello it is use effect')
+    axios.get(`https://fakestoreapi.com/products`)
+    .then(res=>{ //res is name it may be any
+      console.log(res.data)
+      setProducts(res.data)
+    })
+    .catch(err=>console.log(err))
+  },[]) // empty array is passed. it dont load when state is changed
   return (
     <>
       <Card />
+      {products.map(item=>{
+        return(
+          <h1>{item.title}</h1>
+        )
+      })}
     </>
   )
 }
