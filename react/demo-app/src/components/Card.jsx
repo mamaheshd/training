0d26b66ab4377eb722340d6f7e09806e9cec9d99
memaheshd <memaheshd@gmail.com>
2023-07-24 14:52:25 +0545

    card is rearranged

--- react/demo-app/src/components/Card.jsx
+++ react/demo-app/src/components/Card.jsx
@@ -7,38 +7,11 @@ const Card = () => {
                 <div className="row row-cols-1 row-cols-md-4 g-4">
                     <div className="col">
                         <div className="card">
-                            <img src="/images/1.jpg" className="card-img-top" alt="..."/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <button className='btn btn-success'> View Details</button>
-                                </div>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card">
-                            <img src="/images/2.jpg" className="card-img-top" alt="..."/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <button className='btn btn-success'> View Details</button>
-                                </div>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card">
-                            <img src="/images/10.jpg" className="card-img-top" alt="..."/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <button className='btn btn-success'> View Details</button>
-                                </div>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card">
-                            <img src="/images/11.jpg" className="card-img-top" alt="..."/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <button className='btn btn-success'> View Details</button>
-                                </div>
+                            <img src="/images/1.jpg" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <button className='btn btn-success'> View Details</button>
+                            </div>
                         </div>
                     </div>
                 </div>
