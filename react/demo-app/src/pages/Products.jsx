1d536a2205e87f12c5f80be36fbef1a151024725
memaheshd <memaheshd@gmail.com>
2023-07-25 15:00:05 +0545

    data is display 12 at first with and limited title name with less and show more option

--- react/demo-app/src/pages/Products.jsx
+++ react/demo-app/src/pages/Products.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 const Products = () => {
   const [products, setProducts] = useState([])
+  const [limit,setLimit]=useState(12)
   // products is variable and 
   // setProducts is function it name may be different
   useEffect(() => {//sideeffect that seen when page is loading in local
@@ -19,9 +20,19 @@ const Products = () => {
     <>
       <div className="contaner_fluid">
         <div className="row row-cols-1 row-cols-md-4 g-4">
-          {products.map((item, i) => (
+          {products.slice(0,limit).map((item, i) => (
             <Card data={item} key={i} /> //data is props
           ))}
+        </div>
+        <div className="d-flex justify-content-center my-3">
+          {limit<products.length&&
+          <button className='btn btn-warning' onClick={()=>setLimit(limit+4)
+          }>Load more</button>
+          } &nbsp;
+          {limit>12&&
+          <button className='btn btn-warning' onClick={()=>setLimit(limit-4)
+          }>Show less</button> 
+          }
         </div>
       </div>
       {/* {products.map(item=>{
