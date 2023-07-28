2987d87c245f45e238078fc669a0f09f75cbb98b
memaheshd <memaheshd@gmail.com>
2023-07-28 15:04:31 +0545

    increase decrease and delete button makes to work in cart.jsx files

--- react/demo-app/src/pages/Cart.jsx
+++ react/demo-app/src/pages/Cart.jsx
@@ -10,6 +10,37 @@ const Cart = () => {
         const cartData = JSON.parse(localStorage.getItem('cart'))
         setProducts(cartData)
     }, [])
+    // decrease Quentity
+    const decreaseQty = id => {
+        const updateProduct = products.map(item => {
+            if (item.id === id && item.quantity > 1) {
+                return { ...item, quantity: item.quantity - 1 }
+            }
+            return item
+        })
+        setProducts(updateProduct)
+        localStorage.setItem('cart', JSON.stringify(updateProduct))
+    }
+
+    // increase quentity
+    const increaseQty = id => {
+        const updateProduct = products.map(item => {
+            if (item.id === id) {
+                return { ...item, quantity: item.quantity + 1 }
+            }
+            return item
+        })
+        setProducts(updateProduct)
+        localStorage.setItem('cart', JSON.stringify(updateProduct))
+    }
+    // remove from cart 
+    const removeCartHandler = (id, name) => {
+        const filterCart=products.filter(item=>item.id !==id)
+        localStorage.setItem('cart',JSON.stringify(filterCart))
+        setProducts(filterCart)
+        toast.success(`${name} is removed from the cart`)
+    }
+
     return (
         <>
             <ToastContainer theme='colored' position='top-center' />
@@ -26,7 +57,7 @@ const Cart = () => {
                                         Your cart Items
                                     </h2>
                                     <div className="col-md-8 shadow">
-                                        {products.map((item,i)=>{
+                                        {products.map((item, i) => (
                                             <Fragment key={i}>
                                                 <hr />
                                                 <div className="row d-flex align-items-center">
@@ -40,21 +71,28 @@ const Cart = () => {
                                                         <span className="text-warning">${item.price}</span>
                                                     </div>
                                                     <div className="col-3">
-                                                        <button className="btn btn-danger">-</button>
-                                                        &nbps;
+                                                        <button className="btn btn-primary" onClick={() => decreaseQty(item.id)}>-</button>
+                                                        &nbsp;
                                                         <span>{item.quantity}</span>
-                                                        &nbps;
-                                                        <button className="btn btn-danger">+</button>
+                                                        &nbsp;
+                                                        <button className="btn btn-primary" onClick={() => increaseQty(item.id)}>+</button>
                                                     </div>
                                                     <div className="col-1">
-                                                        <button className="btn btn-danger"><FaTrash/></button>
+                                                        <button className="btn btn-danger" onClick={() => removeCartHandler(item.id, item.title)}><FaTrash /></button>
                                                     </div>
                                                 </div>
                                             </Fragment>
-                                        })}
+                                        ))}
                                     </div>
                                     <div className="col-md-3">
-
+                                        <div className="shadow p-2">
+                                            <h5>Cart summary</h5>
+                                            <hr />
+                                            <p><strong>Units:</strong>{products.reduce((ac, item) => (ac + Number(item.quantity)), 0)}</p>
+                                            <p><strong>Total:</strong>${products.reduce((ac, item) => (ac + item.quantity * item.price), 0)}</p>
+                                            <hr />
+                                            <button className="btn btn-warning">Check Out</button>
+                                        </div>
                                     </div>
                                 </Fragment>
                             )
