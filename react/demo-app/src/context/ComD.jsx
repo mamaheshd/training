7d5ee4b336187cf5fe034cb790dd0870e0bc4804
memaheshd <memaheshd@gmail.com>
2023-08-01 20:00:34 +0545

    passing objects as global function and pass to context

--- react/demo-app/src/context/ComD.jsx
+++ react/demo-app/src/context/ComD.jsx
@@ -4,9 +4,9 @@ const ComD = () => {
     const data = useContext(GlobalContext)
     return (
         <>
-            <h2>The technology we are learning is  {data}
-
+            <h2>The name of the student is  {data.fName} {data.lName}.
             </h2>
+            <h2>He lives in {data.address}.</h2>
         </>
     )
 }
--- react/demo-app/src/context/Global.js
+++ react/demo-app/src/context/Global.js
@@ -3,8 +3,13 @@ import React, { createContext } from "react";
 export const GlobalContext = createContext()
 
 const GlobalContextProvider = (props) => {
+    const student={
+        fName:'Mahesh',
+        lName:'Dahal',
+        address:'Kathmandu'
+    }
     return (
-        <GlobalContext.Provider value={'React Js'}>
+        <GlobalContext.Provider value={student}>
             {props.children}
         </GlobalContext.Provider>
     )
